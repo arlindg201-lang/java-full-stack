794271efd39444f1359837f8120095cc0cc0e8a0
arlindg201-lang <arlindg201@gmail.com>
2026-07-07 14:28:28 +0800

    java学习第一天提交

---
+++ java-base-01/src/com/code/day01/Demo01.java
@@ -0,0 +1,17 @@
+package com.code.day01;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Demo01 {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("1", "2");
+
+        List<String> result = list.stream().filter(x -> Integer.parseInt(x) > 1).collect(Collectors.toList());
+        System.out.println(result);
+
+    }
+}
---
+++ java-base-01/src/com/code/day02/Main.java
@@ -0,0 +1,12 @@
+package com.code.day02;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hello java");
+
+    }
+}
