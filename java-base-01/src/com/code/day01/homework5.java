7c9a1b3af1668b99f95f7613438b2e86054091bc
arlindg201-lang <arlindg201@gmail.com>
2026-07-08 18:59:17 +0800

    JAVA的基础语法 第一天作业

--- java-base-01/src/com/code/day01/homework5.java
+++ java-base-01/src/com/code/day01/homework5.java
@@ -4,9 +4,9 @@ public class homework5 {
 
 
 
-    (1)byte,short,int,long,float,double,char,boolean
-    String不是数据类，是个引用类型
+    //(1)byte,short,int,long,float,double,char,boolean
+    //String不是数据类，是个引用类型
 
 
-            (2)char 只能储存一个汉字
+          //  (2)char 只能储存一个汉字
 }
